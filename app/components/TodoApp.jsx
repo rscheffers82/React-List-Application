0401f67a966581d56a4092f3439b4197de41662e
Roy Scheffers <r.scheffers@gmail.com>
2016-11-27 20:35:07 -0500

    Add toggleComplete functionality

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -12,16 +12,20 @@ var TodoApp = React.createClass({
       todos: [
         {
           id: uuid(),
-          text: 'Walk the dog'
+          text: 'Walk the dog',
+          completed: false
         }, {
           id: uuid(),
-          text: 'Help Zoki'
+          text: 'Help Zoki',
+          completed: true
         }, {
           id: uuid(),
-          text: 'Clean room'
+          text: 'Clean room',
+          completed: true
         }, {
           id: uuid(),
-          text: 'Cook dinner'
+          text: 'Cook dinner',
+          completed: false
         }
       ]
     };
@@ -32,7 +36,8 @@ var TodoApp = React.createClass({
         ...this.state.todos,
         {
           id: uuid(),
-          text: text
+          text: text,
+          completed: false
         }
       ]
     });
@@ -43,6 +48,15 @@ var TodoApp = React.createClass({
       searchText: searchText.toLowerCase()
     });
   },
+  handleToggle: function(id){
+    var updatedTodos = this.state.todos.map( (todo) => {
+      if (todo.id === id){
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    this.setState({ todos: updatedTodos });
+  },
   render: function () {
     var {todos} = this.state;
 
@@ -50,7 +64,7 @@ var TodoApp = React.createClass({
       <div>
         <h1>React ToDo App</h1>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     );
