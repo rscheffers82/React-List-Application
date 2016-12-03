6ad4f690c06e58ae9688868fc778d2fdba23cb41
Roy Scheffers <r.scheffers@gmail.com>
2016-12-03 00:25:06 -0500

    Redux integration, actions added

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,8 @@ module.exports = {
       './app/api'
     ],
     alias: {
-      applicationStyles: 'app/styles/app.scss'
+      applicationStyles: 'app/styles/app.scss',
+      actions: 'app/actions/actions.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
