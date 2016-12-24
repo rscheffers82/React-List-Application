2cb7b9cde35e66bf5f327f56d327d24a63b4edf2
Roy Scheffers <r.scheffers@gmail.com>
2016-12-24 16:59:18 -0600

    Possible bug fix for foundation on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ try {
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
-    'script!foundation-sites/dist/foundation.min.js',
+    'script!foundation-sites/dist/js/foundation.min.js',
     './app/app.jsx'
   ],
   externals: {
